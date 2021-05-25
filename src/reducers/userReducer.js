5b98ceebda2316d0b025d7275a7e879ee16ec965
Benmissi-A <aminebenmissi@hotmail.fr>
2021-05-25 10:18:33 +0200

    add UserReducer

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,36 @@
+// reducers/userReducer.js
+// actions: LOGIN_INIT, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT
+
+export const userReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN_INIT":
+      return {
+        ...state,
+        loading: true,
+        error: "",
+      }
+    case "LOGIN_FAILURE":
+      return {
+        ...state,
+        user: null,
+        loading: false,
+        error: action.payload,
+      }
+    case "LOGIN_SUCCESS":
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+        error: "",
+      }
+    case "LOGOUT":
+      return {
+        ...state,
+        user: null,
+        loading: false,
+        error: "",
+      }
+    default:
+      throw new Error(`Unsupported action type ${action.type} in userReducer`)
+  }
+}
